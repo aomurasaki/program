549b2d84ba50d0e406ae0d227a327231c0faf2d2
aomurasaki <aomurasaki_rooling_sord@yahoo.co.jp>
2014-01-11 18:35:51 +0900

    added

--- abc.c
+++ abc.c
@@ -5,9 +5,14 @@ int main ()
 	char s[100];
 	
 	puts("input plz");
-	fgets(s,80,stdin);
+	fgets(s,100,stdin);
+
+	for(int i; i < 100; i++)
+	{
+		if(s[i] == " ") s[i] = i++;
+		printf(s);	
+	}
 	
-	printf(s);
 
 	return 0;
 
