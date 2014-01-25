77a1cef1f6f7eeed0241ae59fe309af49dfd56fd
aomurasaki <aomurasaki_rooling_sord@yahoo.co.jp>
2014-01-25 18:26:33 +0900

    kuku

--- kuku.c
+++ kuku.c
@@ -5,13 +5,26 @@ int main()
 {	
 	int i = 0;
 	int j = 0;
-
+	
+	printf("<!DOCTYPE html>\n");
+	printf("<html>\n");
+	printf("  <head>\n");
+	printf("    <meta charset='UTF-8'>\n");
+	printf("    <title>9x9</title>\n");
+	printf("  </head>\n");
+	printf("  <body>\n");
+	printf("  <table>\n");
 	for(i = 0 ;i < 9; i++)
 	{
+		printf("  <tr>\n");
 		for(j = 0; j < 9; j++)
-		{
-			printf("%d\n",(i+1)*(j+1));
+		{			
+			printf("         %d\n",(i+1)*(j+1));
 		}
+		printf("  </tr>\n");
 	}
+	printf("  </table>\n");
+	printf(" </body>\n");
+	printf("</html>\n");
 	return 0;
 }
